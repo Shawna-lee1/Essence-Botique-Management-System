91515e6eb13a6327288d5395380c2db99181f579
Kyra Young <kyrayoung124@gmail.com>
2023-11-15 16:53:40 -0500

    Update insert_values.sql

--- insert_values.sql
+++ insert_values.sql
@@ -15,4 +15,189 @@ INSERT INTO products VALUES('Boyfriend jeans for women', 'This denim pants has c
 INSERT INTO products VALUES('3 Piece Bikini Set', 'Perfect for tropical vacations, summer, beach & pool, honeymoon, cruise.', 30.99, 'S, M, L', 'olive green, hot pink, lavender, blue', 'Shein', 89);
 INSERT INTO products VALUES('2 piece Thong bikini set for women', 'Cheeky thong bathing suits features small ring metal accessories, side adjustable spaghetti straps , moderate butt coverage, high cut, high leg, make you look more charming and sexy.', 23.99, 'S, M, L', 'purple, yellow, dark green, coffee, red', 'Shein', 129);
 
---values for 
+--values for categories table
+INSERT INTO categories VALUES(1, 'Tops');
+INSERT INTO categories VALUES(2, 'Dress');
+INSERT INTO categories VALUES(3, 'Tops');
+INSERT INTO categories VALUES(4, 'Tops');
+INSERT INTO categories VALUES(5, 'Dress');
+INSERT INTO categories VALUES(6, 'Bottom');
+INSERT INTO categories VALUES(7, 'Accessories');
+INSERT INTO categories VALUES(8, 'Bottom');
+INSERT INTO categories VALUES(9, 'Tops');
+INSERT INTO categories VALUES(10, 'Accessories');
+INSERT INTO categories VALUES(11, 'Jumpsuit');
+INSERT INTO categories VALUES(12, 'Tops');
+INSERT INTO categories VALUES(13, 'Bottom');
+INSERT INTO categories VALUES(14, 'Beach wear');
+INSERT INTO categories VALUES(15, 'Beach wear');
+
+--values for product_category
+INSERT INTO product_category VALUES(1);
+INSERT INTO product_category VALUES(2);
+INSERT INTO product_category VALUES(3);
+INSERT INTO product_category VALUES(4);
+INSERT INTO product_category VALUES(5);
+INSERT INTO product_category VALUES(6);
+INSERT INTO product_category VALUES(7);
+INSERT INTO product_category VALUES(8);
+INSERT INTO product_category VALUES(9);
+INSERT INTO product_category VALUES(10);
+INSERT INTO product_category VALUES(11);
+INSERT INTO product_category VALUES(12);
+INSERT INTO product_category VALUES(13);
+INSERT INTO product_category VALUES(14);
+INSERT INTO product_category VALUES(15);
+
+--values for admin
+insert into admin (id, user_name, password) values (1, 'ebugdall0', 'zV2#MwXl');
+insert into admin (id, user_name, password) values (2, 'tdabner1', 'qB2#31@W=.YT');
+insert into admin (id, user_name, password) values (3, 'rwickerson2', 'yD3{/ex''');
+insert into admin (id, user_name, password) values (4, 'dpea3', 'eS7$@Z8ATC5');
+insert into admin (id, user_name, password) values (5, 'mglossup4', 'gX2,p3?z@Exr');
+insert into admin (id, user_name, password) values (6, 'ocrollman5', 'rH2?#)}r8gT8LZGq');
+insert into admin (id, user_name, password) values (7, 'cdummer6', 'iL5*8%_i*Rl@!q+');
+insert into admin (id, user_name, password) values (8, 'jbuxey7', 'eE1(Ju9=NZqV"Q`<');
+insert into admin (id, user_name, password) values (9, 'cgetch8', 'bG5`T,Vs$pCH)+');
+insert into admin (id, user_name, password) values (10, 'pluisetti9', 'nF8`FX5,0''Y,B');
+insert into admin (id, user_name, password) values (11, 'tbalmadiera', 'yC9=i6g9HjHn');
+insert into admin (id, user_name, password) values (12, 'ssibbitb', 'dS8*#6o<');
+insert into admin (id, user_name, password) values (13, 'cpileticc', 'mT4@U&C<roSqc');
+insert into admin (id, user_name, password) values (14, 'jstrettelld', 'wN2@%Y_`#S');
+insert into admin (id, user_name, password) values (15, 'daldinee', 'nR5''T>N?}n=W,{');
+
+--values for customers
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (1, 'Saw', 'Bercher', 'sbercher0@acquirethisname.com', 'bI8!|+lvUN0W', '21 Waywood Terrace', '651-562-1369', 'jcb');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (2, 'Jervis', 'Alves', 'jalves1@friendfeed.com', 'sS6\u8$SJ', '603 Loeprich Junction', '413-537-5688', 'bankcard');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (3, 'Natividad', 'Down', 'ndown2@nba.com', 'tU6\3rMrPVFXI5', '3925 Rusk Trail', '892-621-1187', 'maestro');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (4, 'Gawen', 'Godmer', 'ggodmer3@booking.com', 'pS3!#`eW"', '7247 Steensland Way', '581-673-0714', 'jcb');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (5, 'Juliane', 'Basilone', 'jbasilone4@dell.com', 'aK5<~ADIv0)''QH"', '2 Westerfield Crossing', '132-761-8446', 'jcb');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (6, 'Gretel', 'Carlesi', 'gcarlesi5@ow.ly', 'pE8'',''Y<Axs|J,', '161 Novick Park', '713-359-5944', 'americanexpress');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (7, 'Pattie', 'Sterman', 'psterman6@yellowbook.com', 'yC1~HXqc', '76 Bayside Point', '349-932-3282', 'visa-electron');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (8, 'Stormi', 'Cains', 'scains7@simplemachines.org', 'jZ6#hE\8qKFdk/', '384 Summerview Court', '619-227-2103', 'jcb');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (9, 'Catharine', 'Sebyer', 'csebyer8@tuttocitta.it', 'dG8.lZ=25L', '0766 Stang Parkway', '327-338-4509', 'americanexpress');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (10, 'Flora', 'Jollie', 'fjollie9@bandcamp.com', 'fW1(6{~E', '69 6th Avenue', '347-238-7123', 'jcb');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (11, 'Jordana', 'Broke', 'jbrokea@japanpost.jp', 'qH1}vg@qzf', '5 Chive Road', '569-548-3360', 'jcb');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (12, 'Dalila', 'Roder', 'droderb@cocolog-nifty.com', 'qM4?pk`f_}j', '28372 Jenna Hill', '259-574-3616', 'visa-electron');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (13, 'Dotti', 'MacCosty', 'dmaccostyc@reddit.com', 'nK6<NBnroQ%KH', '9834 Independence Drive', '265-599-2599', 'switch');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (14, 'Shelagh', 'McBeth', 'smcbethd@gov.uk', 'uV0?U@T=.y', '8 Maryland Drive', '519-352-3267', 'jcb');
+insert into customers (id, first_name, last_name, email, password, street_address, telephone, payment_info) values (15, 'Gery', 'Bafford', 'gbafforde@ezinearticles.com', 'zQ5|(=8Y/OA', '1630 Lighthouse Bay Way', '979-370-7715', 'china-unionpay');
+
+--values for wishlist
+INSERT INTO wishlist VALUES(1);
+INSERT INTO wishlist VALUES(2);
+INSERT INTO wishlist VALUES(3);
+INSERT INTO wishlist VALUES(4);
+INSERT INTO wishlist VALUES(5);
+INSERT INTO wishlist VALUES(6);
+INSERT INTO wishlist VALUES(7);
+INSERT INTO wishlist VALUES(8);
+INSERT INTO wishlist VALUES(9);
+INSERT INTO wishlist VALUES(10);
+INSERT INTO wishlist VALUES(11);
+INSERT INTO wishlist VALUES(12);
+INSERT INTO wishlist VALUES(13);
+INSERT INTO wishlist VALUES(14);
+INSERT INTO wishlist VALUES(15);
+
+--values for coupons
+insert into coupons (id, code, discount_amount, expiration_date) values (1, '4253389473', '$0.06', '6/14/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (2, '4845158221', '$1.64', '9/20/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (3, '3710692342', '$11.81', '2/9/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (4, '4575391336', '$17.80', '3/1/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (5, '1779283935', '$10.17', '1/20/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (6, '1838283935', '$8.36', '5/28/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (7, '0005480809', '$17.26', '2/21/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (8, '18276895443', '$1.70', '11/13/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (9, '4396431716', '$1.83', '12/9/2023');
+insert into coupons (id, code, discount_amount, expiration_date) values (10, '4018150687', '$11.23', '5/18/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (11, '4934226079', '$14.69', '9/25/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (12, '2767808942', '$2.86', '7/10/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (13, '1636226079', '$11.36', '9/4/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (14, '3026595373', '$16.35', '11/19/2024');
+insert into coupons (id, code, discount_amount, expiration_date) values (15, '2624382089', '$5.97', '4/24/2024');
+
+--values for orders
+insert into orders (id, order_date, status, total_number_of_items) values (1, '7/22/2024', null, 76);
+insert into orders (id, order_date, status, total_number_of_items) values (2, '8/25/2024', null, 87);
+insert into orders (id, order_date, status, total_number_of_items) values (3, '3/12/2024', null, 60);
+insert into orders (id, order_date, status, total_number_of_items) values (4, '7/8/2024', null, 15);
+insert into orders (id, order_date, status, total_number_of_items) values (5, '7/20/2024', null, 57);
+insert into orders (id, order_date, status, total_number_of_items) values (6, '5/13/2024', null, 20);
+insert into orders (id, order_date, status, total_number_of_items) values (7, '4/8/2024', null, 54);
+insert into orders (id, order_date, status, total_number_of_items) values (8, '10/1/2024', null, 4);
+insert into orders (id, order_date, status, total_number_of_items) values (9, '3/9/2024', null, 66);
+insert into orders (id, order_date, status, total_number_of_items) values (10, '1/20/2024', null, 41);
+insert into orders (id, order_date, status, total_number_of_items) values (11, '12/4/2023', null, 23);
+insert into orders (id, order_date, status, total_number_of_items) values (12, '5/3/2024', null, 58);
+insert into orders (id, order_date, status, total_number_of_items) values (13, '11/21/2023', null, 15);
+insert into orders (id, order_date, status, total_number_of_items) values (14, '2/15/2024', null, 71);
+insert into orders (id, order_date, status, total_number_of_items) values (15, '2/8/2024', null, 30);
+
+--values for ordered_items
+insert into ordered_items (id, quantity, subtotal) values (1, 46, '$49.25');
+insert into ordered_items (id, quantity, subtotal) values (2, 8, '$16.63');
+insert into ordered_items (id, quantity, subtotal) values (3, 48, '$15.51');
+insert into ordered_items (id, quantity, subtotal) values (4, 91, '$48.54');
+insert into ordered_items (id, quantity, subtotal) values (5, 47, '$27.39');
+insert into ordered_items (id, quantity, subtotal) values (6, 57, '$21.13');
+insert into ordered_items (id, quantity, subtotal) values (7, null, '$14.88');
+insert into ordered_items (id, quantity, subtotal) values (8, 30, '$16.51');
+insert into ordered_items (id, quantity, subtotal) values (9, 15, '$36.50');
+insert into ordered_items (id, quantity, subtotal) values (10, 17, '$36.09');
+insert into ordered_items (id, quantity, subtotal) values (11, null, '$22.25');
+insert into ordered_items (id, quantity, subtotal) values (12, 40, '$18.35');
+insert into ordered_items (id, quantity, subtotal) values (13, null, '$34.17');
+insert into ordered_items (id, quantity, subtotal) values (14, 82, '$43.42');
+insert into ordered_items (id, quantity, subtotal) values (15, 89, '$30.41');
+
+--values for address
+insert into address (id, street_address, city, state, postal_code, country) values (1, '696 Acker Way', 'Abilene', 'TX', '79605', 'United States');
+insert into address (id, street_address, city, state, postal_code, country) values (2, '418 Lakewood Point', 'Vitrolles', 'B8', '13841 CEDEX', 'France');
+insert into address (id, street_address, city, state, postal_code, country) values (3, '6 Hansons Center', 'Pasadena', 'CA', '91199', 'United States');
+insert into address (id, street_address, city, state, postal_code, country) values (4, '21595 Gina Road', 'Amarillo', 'TX', '79165', 'United States');
+insert into address (id, street_address, city, state, postal_code, country) values (5, '23618 Memorial Point', 'Amarillo', 'TX', '79118', 'United States');
+insert into address (id, street_address, city, state, postal_code, country) values (6, '87 Sunnyside Park', 'Aurora', 'CO', '80045', 'United States');
+insert into address (id, street_address, city, state, postal_code, country) values (7, '1292 Drewry Circle', 'Idaho Falls', 'ID', '83405', 'United States');
+insert into address (id, street_address, city, state, postal_code, country) values (8, '06894 Waubesa Crossing', 'Colorado Springs', 'CO', '80910', 'United States');
+insert into address (id, street_address, city, state, postal_code, country) values (9, '4 Hovde Drive', 'Miami', 'FL', '33245', 'United States');
+insert into address (id, street_address, city, state, postal_code, country) values (10, '631 Springs Pass', 'Carcavelos', '13', '4620-031', 'Portugal');
+insert into address (id, street_address, city, state, postal_code, country) values (11, '9573 Pierstorff Street', 'Charleston', 'WV', '25326', 'United States');
+insert into address (id, street_address, city, state, postal_code, country) values (12, '17 Fordem Pass', 'Spokane', 'WA', '99210', 'United States');
+insert into address (id, street_address, city, state, postal_code, country) values (13, '9 Northport Avenue', 'Wissembourg', 'C1', '67165 CEDEX', 'France');
+insert into address (id, street_address, city, state, postal_code, country) values (14, '13602 Washington Plaza', 'Shawnee Mission', 'KS', '66225', 'United States');
+insert into address (id, street_address, city, state, postal_code, country) values (15, '64 Sherman Terrace', 'Saint Paul', 'MN', '55127', 'United States');
+
+--values for shipping_info
+insert into shipping_info (id, shipping_method, tracking_number) values (1, 'Fedex Ground', '0031596029');
+insert into shipping_info (id, shipping_method, tracking_number) values (2, 'Fedex Ground', '2481208943');
+insert into shipping_info (id, shipping_method, tracking_number) values (3, 'Fedex Ground', '4567739124');
+insert into shipping_info (id, shipping_method, tracking_number) values (4, 'Fedex Ground', '8621184265');
+insert into shipping_info (id, shipping_method, tracking_number) values (5, 'Fedex Ground', '8560596046');
+insert into shipping_info (id, shipping_method, tracking_number) values (6, 'Fedex Ground', '6422039298');
+insert into shipping_info (id, shipping_method, tracking_number) values (7, 'Fedex Ground', '9256175533');
+insert into shipping_info (id, shipping_method, tracking_number) values (8, 'Fedex Ground', '8287649412');
+insert into shipping_info (id, shipping_method, tracking_number) values (9, 'Fedex Ground', '7945230857');
+insert into shipping_info (id, shipping_method, tracking_number) values (10, 'Fedex Ground', '6936682960');
+insert into shipping_info (id, shipping_method, tracking_number) values (11, 'Fedex Ground', '6972574528');
+insert into shipping_info (id, shipping_method, tracking_number) values (12, 'Fedex Ground', '8183353266');
+insert into shipping_info (id, shipping_method, tracking_number) values (13, 'Fedex Ground', '0647564408');
+insert into shipping_info (id, shipping_method, tracking_number) values (14, 'Fedex Ground', '7443137613');
+insert into shipping_info (id, shipping_method, tracking_number) values (15, 'Fedex Ground', '2422704492');
+
+--values for reviews
+insert into reviews (id, rating, comment, review_date) values (1, 4.25, null, '9/28/2024');
+insert into reviews (id, rating, comment, review_date) values (2, 4.7, null, '8/1/2024');
+insert into reviews (id, rating, comment, review_date) values (3, 3.81, null, '1/2/2024');
+insert into reviews (id, rating, comment, review_date) values (4, 3.52, null, '5/22/2024');
+insert into reviews (id, rating, comment, review_date) values (5, 3.45, null, '10/5/2024');
+insert into reviews (id, rating, comment, review_date) values (6, 3.81, null, '11/7/2024');
+insert into reviews (id, rating, comment, review_date) values (7, 3.47, null, '12/1/2023');
+insert into reviews (id, rating, comment, review_date) values (8, 3.76, null, '2/13/2024');
+insert into reviews (id, rating, comment, review_date) values (9, 3.92, null, '7/16/2024');
+insert into reviews (id, rating, comment, review_date) values (10, 4.35, null, '5/7/2024');
+insert into reviews (id, rating, comment, review_date) values (11, 3.09, null, '6/15/2024');
+insert into reviews (id, rating, comment, review_date) values (12, 3.4, null, '10/19/2024');
+insert into reviews (id, rating, comment, review_date) values (13, 3.72, null, '3/3/2024');
+insert into reviews (id, rating, comment, review_date) values (14, 4.80, null, '10/3/2024');
+insert into reviews (id, rating, comment, review_date) values (15, 3.81, null, '8/3/2024');
