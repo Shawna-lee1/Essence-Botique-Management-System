7c159c39ec24865b93164b50f62f925c5bb1e8c6
Shawna-lee1 <104038577+Shawna-lee1@users.noreply.github.com>
2023-12-02 16:06:26 -0500

    Add files via upload

---
+++ Essence_Webpage/mainpage.js
@@ -0,0 +1,29 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("jacket-slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+// Optional: Auto change slides
+setInterval(function() { plusSlides(1); }, 4000); // Change every 4 seconds
