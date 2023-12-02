d52d7d7afe08a4aca4e5b002909d47d7d6d62733
Kyra Young <kyrayoung124@gmail.com>
2023-12-02 16:15:28 -0500

    Update test-queries.sql

--- test-queries.sql
+++ test-queries.sql
@@ -1,5 +1,38 @@
---Run this query to see what countries our customers live in.
+--QUERY #1  Run this query to see what countries our customers live in.
 SELECT DISTINCT country
 FROM address;
 
---
+--QUERY #2
+UPDATE coupons 
+SET discount_amount = 2
+WHERE code = 4575391336;
+
+--QUERY #3
+UPDATE address
+SET state = 'FR'
+WHERE country = 'France' 
+returning state as state_id;
+
+--QUERY #4
+DELETE FROM reviews 
+WHERE rating < 3.5;
+
+--QUERY #5
+SELECT comment, rating
+FROM reviews
+WHERE review_date between '04-01-2024'
+  AND '05-30-2024';
+
+--QUERY #6
+SELECT count(*)
+FROM products;
+
+--QUERY #7
+SELECT *  
+FROM ordered_items
+  INNER JOIN  orders
+  ON  orders.id = ordered_items.id;
+
+--QUERY #8
+insert into coupons (id, code, discount_amount, expiration_date) 
+  values (16, '4934226079', '$14.69', '9/25/2024');
